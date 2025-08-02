30099a439f502c72669b1f5a0b72368fd75192db
vasily <121089045+cnwhli@users.noreply.github.com>
2025-08-02 21:49:58 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -114,6 +114,16 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
+    ckzy: {
+         api: 'https://www.ckzy1.com',
+         name: 'CK资源',
+         adult: true
+     },
+     jkun: {
+         api: 'https://jkunzyapi.com',
+         name: 'jkun资源',
+         adult: true
+     },
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
